9ec80e248c916e7e11bde9df54cdd0ff483d2536
[Bartosz Bala] <[bala.bartosz@gmail.com]>
2019-12-11 00:26:31 +0100

    commit 3

--- src/main/java/MainSolution.java
+++ src/main/java/MainSolution.java
@@ -8,34 +8,62 @@ public class MainSolution {
         int right = 1;
 
 
+        boolean checkNorth = false;
+        boolean isNorthTheSameCountry = false;
+        boolean isEastTheSameCountry = false;
+
+
         for (int i = 0; i < n; i++) {
 
-
+            if (i != 0)
+                checkNorth = true;
             for (int j = 0; j < m; j++) {
+//
 
-                if (j == 0) {
-                    up = 1;
-                }
+                if (n != 1 && i != 0 && checkNorth && A[i][j] == A[i - up][j]) {
 
-
-                if (n!=1&&i != 0 && up != 0 && A[i][j] == A[i - up][j]) {
 
                     possibleCountries = possibleCountries - 1;
-                    up = 0;
+                    if (i != 0)
+                        checkNorth = false;
+
+
+                }
+
+                if (m != 1 && j != m - 1 && A[i][j] == A[i][j + right]) {
+                    possibleCountries = possibleCountries - 1;
+
 
                 } else {
-                    up = 1;
+                    if (!checkNorth && i != 0)
+                        checkNorth = true;
+
+                }
+
+                if (n!=1&& m!=1 && i + 1 == n && j + 1 == m && A[i][j] == A[i][j - 1] && A[i - 1][j] == A[i][j]) {
+
+                    System.out.println("test");
+
+
+                    if(n==2) {
+
+                        if (A[i - 1][j] != A[i - 1][j - 1]) {
+                            possibleCountries = possibleCountries - 1;
+
+                        }
+
+                    }
+
+                 else   if (A[i - 1][j] != A[i - 1][j - 1] && A[i - 1][j] != A[i - 2][j]) {
+                        possibleCountries=possibleCountries-1;
+
+                    }
+
+
                 }
 
 
-                if (m!=1&&j != m - 1 && A[i][j] == A[i][j + right]) {
-                    possibleCountries = possibleCountries - 1;
-
-
-                }else{
-                    up=1;
-                }
-
+//
 
             }
 
@@ -43,8 +71,6 @@ public class MainSolution {
         return possibleCountries;
 
     }
-
-
 
 
 }
--- src/test/java/MainSolutionTest.java
+++ src/test/java/MainSolutionTest.java
@@ -64,77 +64,97 @@ public class MainSolutionTest {
     @Test
     public void randomTest() {
 
-//        int[][] A9 = {{2,2,2,2,2,2,2}, {2,1,1,1,1,1,2},{2,1,2,2,2,1,2},{2,1,2,2,2,1,2},{2,1,2,2,2,1,2}};
-//
-////        ,{2,1,1,1,1,1,2},{2,2,2,2,2,2,2}
-//        int expected9 = 3;
-//        int actual9 = ms.solution(A9);
 
-//        assertEquals(expected9, actual9);
+
+
+        int[][] A14 = {{1,1,2,3,5,4},{2,3,1,3,4,4}};
+        int expected14 = 8;
+        int actual14 = ms.solution(A14);
+        assertEquals(expected14, actual14);
+
+//        {2,2,2,5,4,3},{5,5,5,5,4,4},{1,1,2,3,1,4},{1,2,1,4,4,4},{1,1,1,4,2,1}
+
+        int[][] A9 = {{2,2,2,2,2,2,2}, {2,1,1,1,1,1,2},{2,1,2,2,2,1,2},{2,1,2,1,2,1,2},{2,1,2,2,2,1,2},{2,1,1,1,1,1,2},{2,2,2,2,2,2,2}};
+        int expected9 = 4;
+        int actual9 = ms.solution(A9);
+        assertEquals(expected9, actual9);
+
+        int[][] A12 = {{1, 1, 1}, {1, 2, 1}};
+        int expected12 = 2;
+        int actual12 = ms.solution(A12);
+        assertEquals(expected12, actual12);
+
+
+        int[][] A13 = {{3,3,3,3,3},{3,1,1, 1, 3}, {3,1, 2, 1,3},{3, 1, 1, 1,3},{3,3,3,3,3}};
+        int expected13 = 3;
+        int actual13 = ms.solution(A13);
+        assertEquals(expected13, actual13);
+
+        int[][] A2 = {{1, 2, 1}, {1, 1, 1}};
+        int expected2 = 2;
+        int actual2 = ms.solution(A2);
+        assertEquals(expected2, actual2);
 
 
         int[][] A10 = {{1,1, 2,2, 3,3}, {1,1, 2,2, 3,3}, {1,1, 2,2, 3,3}};
         int expected10 = 3;
         int actual10 = ms.solution(A10);
-
         assertEquals(expected10, actual10);
 
-        int[][] A0 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        int expected0 = 9;
-        int actual0 = ms.solution(A0);
 
+
+
+
+        int[][] A0 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+       int expected0 = 9;
+     int actual0 = ms.solution(A0);
         assertEquals(expected0, actual0);
 
         int[][] A1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 8}, {9, 10, 8}};
         int expected1 = 10;
         int actual1 = ms.solution(A1);
-
         assertEquals(expected1, actual1);
 
 
-        int[][] A2 = {{1, 2, 1}, {1, 1, 1}};
-        int expected2 = 2;
-        int actual2 = ms.solution(A2);
 
-        assertEquals(expected2, actual2);
 
         int[][] A3 = {{3, 1, 2, 1, 1}, {1, 1, 2, 1, 1}, {2, 2, 2, 2, 2}};
         int expected3 = 4;
         int actual3 = ms.solution(A3);
-
         assertEquals(expected3, actual3);
 
 
         int[][] A4 = {{2, 1, 2, 1, 1}, {1, 1, 2, 1, 1}, {2, 2, 2, 2, 2}, {1, 1, 2, 1, 1}, {1, 1, 2, 1, 1}};
         int expected4 = 6;
         int actual4 = ms.solution(A4);
-
         assertEquals(expected4, actual4);
 
 
         int[][] A5 = {{1, 2, 2, 1, 1}, {1, 1, 2, 1, 1}, {2, 2, 2, 2, 2}, {1, 1, 2, 6, 5}, {1, 1, 2, 3, 4}};
         int expected5 = 8;
         int actual5 = ms.solution(A5);
-
         assertEquals(expected5, actual5);
 
         int[][] A6 = {{1, 3, 3, 3}, {3, 1, 3, 3}, {3, 3, 1, 3}, {3, 3, 3, 1}};
         int expected6 = 6;
         int actual6 = ms.solution(A6);
-
         assertEquals(expected6, actual6);
 
         int[][] A7 = {{1, 3, 3, 3}, {3, 3, 3, 3}, {3, 3, 1, 3}, {3, 3, 3, 1}};
-        int expected7 = 3;
+        int expected7 = 4;
         int actual7 = ms.solution(A7);
-
         assertEquals(expected7, actual7);
 
         int[][] A8 = {{1, 2}, {2,1}};
         int expected8 = 4;
         int actual8 = ms.solution(A8);
-
         assertEquals(expected8, actual8);
+
+        int[][] A11 = {{1, 1,1,1}, {1,1,2,1},{1, 1,1,1}};
+        int expected11 = 2;
+        int actual11 = ms.solution(A11);
+
+        assertEquals(expected11, actual11);
 
 
 
